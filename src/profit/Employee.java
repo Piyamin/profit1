e0d9cde6a74cf16f407deab5165bc5e0f3d3f21b
Piyamin <piyaminjaima42@gmail.com>
2020-01-11 17:59:56 +0700

    edit classEployee and class Salary

--- src/profit/Employee.java
+++ src/profit/Employee.java
@@ -5,31 +5,22 @@ public class Employee {
 	
 	private String name;
     private String emp_id;
-    
-    public Employee(String name, String emp_id) {
+    public Employee() {
+    }
+    public Employee(String emp_id,String name) {
     	super();
         this.name = name;
         this.emp_id = emp_id;
     }
-    public Employee() {
-    } 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getEmp_id() {
         return emp_id;
     }
 	 
-    public void setEmp_id(String emp_id) {
-        this.emp_id = emp_id;
-    }
     public void printEmployee() {
-        System.out.println("Name:"+ name );
-        System.out.println("emp_id:"+ emp_id );
+        System.out.println(""+ emp_id+"  "+ name  );
     }
 }
--- src/profit/Salary.java
+++ src/profit/Salary.java
@@ -3,4 +3,11 @@ package profit;
 public class Salary {
 	public double salary;
 	public double commission;
+public Salary () {}
+public Salary(double salary) {
+	this.salary=salary;
+}
+public double getSalary() {
+	return salary;
+}
 }
