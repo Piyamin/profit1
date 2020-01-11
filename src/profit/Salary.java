084db79269050ee8c714529eea34f28c04b63eb5
Zzging <chanikan.po@mail.wu.ac.th>
2020-01-12 01:58:27 +0700

    edit class Salary

--- src/profit/Salary.java
+++ src/profit/Salary.java
@@ -1,6 +1,28 @@
 package profit;
 
 public class Salary {
-	public double salary;
-	public double commission;
+	 private double salary;
+	 private double commission;
+	
+	  public double getSalary() {
+	        return salary;
+	  }
+
+	   public double getcommission() {
+	        return commission;
+	    }
+	   public void setSalary(double Salary) {
+	        this.salary = salary;
+	
+	  }
+
+	    public void setcommission(double commission) {
+	        this.commission = commission;
+	    }  
+	    public void printSalary() {
+	    	  System.out.println("Salary:" + salary);
+	    	  System.out.println("Commission:" + commission);
+	    	  
+	        				
+	     }
 }
