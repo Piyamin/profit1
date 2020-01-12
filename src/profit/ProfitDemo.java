106ac5a3e8634bd2d88b7be6dd442a7596ff10d1
Piyamin <piyaminjaima42@gmail.com>
2020-01-12 14:26:29 +0700

    edit ProfitDemo

--- src/profit/ProfitDemo.java
+++ src/profit/ProfitDemo.java
@@ -3,7 +3,10 @@ package profit;
 public class ProfitDemo {
 
 	public static void main(String[] args) {
-		
+	System.out.println("Please select Menu ");
+	System.out.println("[1] Add Employee");
+	System.out.println("[2] Save sales ");
+	
 
 	}
 
